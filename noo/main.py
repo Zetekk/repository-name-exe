c1720eb2bad26bbe7ccd1a8ce7e3ee6d3a8884b2
Zetekk <piotr.zapala@lo31.krakow.pl>
2021-06-06 14:46:28 +0200

    added shooting

--- noo/main.py
+++ noo/main.py
@@ -31,25 +31,48 @@ class Player(pygame.sprite.Sprite):
         self.press_right = False
         self.press_up = False
         self.press_down = False
+        self.press_w = False
+        self.press_a = False
+        self.press_s = False
+        self.press_d = False
+        self.direction_pressed = 0
+        self.lifes = 3
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
     def turn_right(self):
-        self.movement_x = 6
+        self.movement_x = 12
 
     def turn_left(self):
-        self.movement_x = -6
+        self.movement_x = -12
 
     def turn_up(self):
-        self.movement_y = -6
+        self.movement_y = -12
 
     def turn_down(self):
-        self.movement_y = 6
+        self.movement_y = 12
     def stop_x(self):
         self.movement_x = 0
     def stop_y(self):
         self.movement_y = 0
 
+    def shoot(self):
+        bullet = Bullet(gm.BULLET,self.direction_pressed, self.rect.centerx, self.rect.centery)
+        self.level.set_of_bullets.add(bullet)
+    #  def shoot_right(self):
+    #    bullet = Bullet(gm.BULLET,  self.rect.centerx, self.rect.centery)
+     #   self.level.set_of_bullets.add(bullet)
+   # def shoot_left(self):
+   #     bullet = Bullet(gm.BULLET,  self.rect.centerx, self.rect.centery)
+   #     self.level.set_of_bullets.add(bullet)
+   # def shoot_up(self):
+   #     bullet = Bullet(gm.BULLET,   self.rect.centerx, self.rect.centery)
+   #     self.level.set_of_bullets.add(bullet)
+   # def shoot_down(self):
+   #     bullet = Bullet(gm.BULLET,  self.rect.centerx, self.rect.centery)
+    #    self.level.set_of_bullets.add(bullet)
+
+#self.press_w, self.press_s,self.press_d, self.press_a,
     def update(self):
         self.rect.x += self.movement_x
         self.rect.y += self.movement_y
@@ -60,7 +83,8 @@ class Player(pygame.sprite.Sprite):
             motion[1] = 0
         self.rect.x += motion[0] * 10
         self.rect.y += motion[1] * 10
-
+        shooty = False
+        timer = 0
         for event in pygame.event.get():
             #if event.type == JOYBUTTONDOWN:
                 #print(event)
@@ -102,6 +126,35 @@ class Player(pygame.sprite.Sprite):
                 if event.key == pygame.K_DOWN:
                     self.press_down = True
                     self.turn_down()
+
+                if event.key == pygame.K_d:
+                    self.press_d = True
+                    self.press_a = False
+                    self.direction_pressed += 10
+                    #self.shoot_right()
+                    #self.shoot()
+                    shooty = True
+                if event.key == pygame.K_w:
+                    self.press_w = True
+                    self.press_s = False
+                    self.direction_pressed += 1
+                    #self.shoot_up()
+                    #self.shoot()
+                    shooty = True
+                if event.key == pygame.K_s:
+                    self.press_s = True
+                    self.press_w = False
+                    self.direction_pressed -= 1
+                   # self.shoot_down()
+                    #self.shoot()
+                    shooty = True
+                if event.key == pygame.K_a:
+                    self.press_a = True
+                    self.press_d = False
+                    self.direction_pressed -= 10
+                    #self.shoot_left()
+                    #self.shoot()
+                    shooty = True
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
                     if self.press_right:
@@ -128,12 +181,79 @@ class Player(pygame.sprite.Sprite):
                         self.stop_y()
                     self.press_down = False
 
+                if event.key == pygame.K_d:
+                    self.press_d = False
+                    self.direction_pressed -= 10
+                if event.key == pygame.K_w:
+                    self.press_w = False
+                    self.direction_pressed -= 1
 
+                if event.key == pygame.K_s:
+                    self.press_s = False
+                    self.direction_pressed += 1
 
+                if event.key == pygame.K_a:
+                    self.press_a = False
+                    self.direction_pressed += 10
+            #print(self.direction_pressed)
+            if shooty == True:
+                #if timer ==0:
+                    #timer+=10
+                if self.direction_pressed == 0:
+                    timer =0
+                else:
+                    self.shoot()
 
+            #timer -=1
+            shooty = False
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, image, direction_pressed, rect_center_x, rec_center_y):
+        super().__init__()
+        self.image= image
+        self.rect = self.image.get_rect()
+        self.direction_pressed = direction_pressed
+        self.rect.center = [rect_center_x, rec_center_y]
+    def update(self):
+        if self.direction_pressed == 10:
+            self.rect.x +=15
+        if self.direction_pressed == 1:
+            self.rect.y -= 15
+        if self.direction_pressed == 11:
+            self.rect.y -= 15
+            self.rect.x += 15
+        if self.direction_pressed == 9:
+            self.rect.x += 15
+            self.rect.y += 15
+        if self.direction_pressed == -10:
+            self.rect.x -=15
+        if self.direction_pressed == -1:
+            self.rect.y += 15
+        if self.direction_pressed == -11:
+            self.rect.y += 15
+            self.rect.x -= 15
+        if self.direction_pressed == -9:
+            self.rect.x -= 15
+            self.rect.y -= 15
 class Level:
     def __init__(self, player):
         self.player = player
+        self.set_of_bullets = pygame.sprite.Group()
+
+    def draw(self, surface):
+        self.set_of_bullets.draw(surface)
+
+        for i in range(player.lifes - 1):
+            surface.blit(gm.HEART, [20 + i * 40, 15])
+    def update(self):
+        self.set_of_bullets.update()
+        self.delete_bullet()
+
+    def delete_bullet(self):
+
+        for b in self.set_of_bullets:
+            if b.rect.left >gm.WIDTH or b.rect.right <0:
+                b.kill()
 
 class Level1(Level):
     def __init__(self, player):
@@ -164,9 +284,9 @@ while window_open:
 
     #rysowanie i aktualizacja obiektów
     player.draw(screen)
-    #current_level.draw(screen)
+    current_level.draw(screen)
     player.update()
-    #current_level.update()
+    current_level.update()
 
     #aktualizacja okna pygame
     pygame.display.flip()
